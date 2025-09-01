fa16395e3446733b9a036ad48a8a35a206656f7f
readwisdomwings <read.wisdomwings@gmail.com>
2025-09-01 21:25:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/readwisdomwings">
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
